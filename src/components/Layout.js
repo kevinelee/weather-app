f9c0ffe55eab295d4cdd7cdacd88b882ef1f03c4
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-09-23 21:47:24 -0700

    starter build completed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,11 +7,11 @@ const Layout = ({ children }) => {
       <SEO />
       <div className="flex flex-col min-h-screen bg-gray-200">
         <header className="p-4 bg-teal-500 text-white font-semibold">
-          <h1 className="max-w-4xl mx-auto">gatsby-starter-tailwind-css</h1>
+          <h1 className="max-w-4xl mx-auto">Basic Weather Application</h1>
         </header>
         {children}
         <footer className="py-2 text-center text-gray-600 text-xs">
-          &copy; Melanie Nolan 2020
+          &copy; Jason Park and Kevin Lee 2020
         </footer>
       </div>
     </>
